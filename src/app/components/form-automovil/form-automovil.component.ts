bfec8315f0307359bb49c597c1dcfd3109a8ef35
piliavilaa <piliavila01@gmail.com>
2022-06-20 14:38:28 -0300

    finalizado sin validaciones

--- src/app/components/form-automovil/form-automovil.component.ts
+++ src/app/components/form-automovil/form-automovil.component.ts
@@ -1,32 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Automovil } from '../../models/automoviles';
+import { AutomovilesService } from '../../services/automoviles.service';
 
 @Component({
   selector: 'app-form-automovil',
   templateUrl: './form-automovil.component.html',
-  styleUrls: ['./form-automovil.component.css']
+  styleUrls: ['./form-automovil.component.css'],
 })
 export class FormAutomovilComponent implements OnInit {
+  constructor(private servicioAutomoviles: AutomovilesService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
-  aceptar(){
+  aceptar() {
+    let auto = {
+      id: 0,
+      Marca: '',
+      Anio: 0,
+      Usado: true,
+    };
     const marcaAutomovil = document.getElementById('Mar') as HTMLInputElement;
     const anio = document.getElementById('anio') as HTMLInputElement;
     var activo = document.getElementById('Usado') as HTMLInputElement;
     var usado = false;
-    
-    if (activo.value == "si")
-    {
+
+    if (activo.value == 'si') {
       usado = true;
     }
 
-    
-
+    this.servicioAutomoviles
+      .post(marcaAutomovil.value, parseInt(anio.value), usado)
+      .subscribe((res: Automovil) => {
+        auto = res;
+        console.log(auto);
+      });
     
   }
-
-
 }
